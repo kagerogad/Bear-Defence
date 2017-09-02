a8f81c44c523caaac920e642113e5248fbb50f85
asantiago33 <asantiago33@gatech.edu>
2017-09-01 23:37:01 -0400

    First Commit

---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour {
+
+	public Color selectedColor;
+
+	Renderer rend;
+	Color startColor;
+
+	void Start() {
+		rend = GetComponent<Renderer> ();
+		startColor = rend.material.color;
+	}
+
+	void OnTriggerEnter(Collider col) {
+		GameObject go = col.gameObject;
+
+		if (go.CompareTag("Player")) {
+			rend.material.color = selectedColor;
+			go.GetComponent<Player> ().ObjectSelected (transform);
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		rend.material.color = startColor;
+		if (col.gameObject.CompareTag("Player")) {
+			col.gameObject.GetComponent<Player> ().ObjectedDeselected ();
+		}
+
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour {
+	//Checking
+	public float speed;
+
+	public Transform holdPosition;
+
+	private Rigidbody playerRB;
+	private Vector3 movement;
+
+	private Transform selectedObject;
+	private bool isObjectSelected;
+	private bool isCarrying;
+
+	private int floorMask;
+	private float camRayLength = 100f;
+
+	//Timers
+	public float startPickUpTimer = 1f;
+	private float pickupTimer;
+
+	void Awake() {
+		floorMask = LayerMask.GetMask ("Floor");
+		playerRB = GetComponent<Rigidbody> ();
+
+		pickupTimer = startPickUpTimer;
+	}
+
+	void FixedUpdate() {
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
+
+		Move (horizontal, vertical);
+		Turn ();
+
+		if (isObjectSelected && !isCarrying && Input.GetKeyDown(KeyCode.E)) {
+			if (pickupTimer == startPickUpTimer) {
+				PickUp ();
+			}
+		} else if (isCarrying && Input.GetKeyDown(KeyCode.E)) {
+			if (pickupTimer == startPickUpTimer) {
+				Drop ();
+			}
+		}
+
+		pickupTimer -= Time.deltaTime;
+	}
+
+	void Move(float horizontal, float vertical) {
+		movement.Set (horizontal, 0f, vertical);
+		movement = movement.normalized * speed * Time.deltaTime;
+
+
+		playerRB.MovePosition (transform.position + movement);
+	}
+
+	void Turn() {
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorHit;
+
+		if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+			playerRB.MoveRotation(newRotation);
+		}
+	}
+
+	void PickUp() {
+		selectedObject.transform.SetParent (holdPosition);
+		isCarrying = true;
+		Debug.Log ("PickedUp");
+	}
+
+	void Drop() {
+		Debug.Log ("Dropped");
+		holdPosition.DetachChildren ();
+		isCarrying = false;
+	}
+
+	public void ObjectSelected(Transform selectedObject) {
+		this.selectedObject = selectedObject;
+		isObjectSelected = true;
+	}
+
+	public void ObjectedDeselected() {
+		selectedObject = null;
+		isObjectSelected = false;
+	}
+
+}
