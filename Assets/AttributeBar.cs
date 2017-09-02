8617f59ac76ad8db276e3e6cdc91aba6eaa3375f
asantiago33 <asantiago33@gatech.edu>
2017-09-02 00:29:26 -0400

    Added AttributeBar
    Added a prefab with a custom editor.

---
+++ Assets/AttributeBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttributeBar : MonoBehaviour {
+
+	public float xScale;
+	public float yScale;
+
+	void Update() {
+		transform.rotation = Quaternion.identity;
+	}
+}
---
+++ Assets/Editor/AttributeBarEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(AttributeBar))]
+public class AttributeBarEditor : Editor {
+
+	public override void OnInspectorGUI ()
+	{
+
+		AttributeBar at = (AttributeBar)target;
+
+		EditorGUI.BeginChangeCheck ();
+		float xScale = EditorGUILayout.Slider ("X Scale", at.xScale, 0.001f, 0.05f);
+		float yScale = EditorGUILayout.Slider ("Y Scale", at.yScale, 0.001f, 0.01f);
+
+		if (EditorGUI.EndChangeCheck()) {
+			Undo.RecordObject (target, "Changed Transform Properties");
+
+			at.xScale = xScale;
+			at.yScale = yScale;
+		}
+
+		at.transform.localScale = new Vector3 (at.xScale, at.yScale, 1f);
+	}
+
+
+
+}
