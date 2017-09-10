30868e573802aaf1aed1ef3054f76b9faf569fb3
asantiago33 <asantiago33@gatech.edu>
2017-09-09 23:50:58 -0400

    Added Space Raptors
    Added raptors as enemies

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public float smoothSpeed;
+	public Transform player;
+
+	public float xOffset;
+	public float yOffset;
+	public float zOffset;
+
+	void FixedUpdate() {
+		Move ();
+	}
+
+	void Move() {
+
+		Vector3 desiredPosition = player.position + new Vector3(xOffset, yOffset, zOffset);
+		Vector3 currentPosition = transform.position;
+
+		Vector3 smoothedPosition = Vector3.Lerp (currentPosition, desiredPosition, smoothSpeed * Time.deltaTime);
+
+		transform.position = smoothedPosition;
+		transform.LookAt (player);
+	}
+
+}
