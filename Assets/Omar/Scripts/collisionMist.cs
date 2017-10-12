bb604cc5fb6abd2113b527d928243becfa890646
Omar Chbaklo <chbakloomar@gmail.com>
2017-10-12 15:41:51 -0400

    Ship parts/ terrain
    Added multiple ship parts
    Added lush terrain (but disabled cuz of fps)
    Added 8 box colliders/ white smoke systems with script that makes system dissapear on impact (must be altered to ask to pay in game currency)

---
+++ Assets/Omar/Scripts/collisionMist.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionMist : MonoBehaviour {
+
+    private Transform mist;
+    private Collider box;
+
+    // Use this for initialization
+    void Start()
+    {
+        mist = transform.GetChild(0);
+        box = GetComponent<Collider>();
+    }
+	// Update is called once per frame
+	void Update () {
+           // mist.position = new Vector3(0f, -200f, 0f);
+        }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        mist.position = new Vector3(0f, -200f, 0f);
+        box.enabled = false;
+    }
+}
