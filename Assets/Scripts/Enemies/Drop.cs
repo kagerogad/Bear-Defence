3f63b9238bc8e725f6e858abd518e6e777341977
asantiago33 <asantiago33@gatech.edu>
2017-10-24 21:02:10 -0400

    Stable commit

--- Assets/Scripts/Enemies/Drop.cs
+++ Assets/Scripts/Enemies/Drop.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Drop : MonoBehaviour {
 
 	public float rotateSpeed = 1f;
+	public float bobSpeed = 1f;
+	public float yLimit = 2f;
 	public float disappearTimer = 3f;
 	public float scrapAmount = 25f;
 
@@ -16,11 +18,21 @@ public class Drop : MonoBehaviour {
 			Destroy (gameObject);
 		}
 		Revolve ();
+		Bob ();
 	}
 
 	void Revolve() {
 		transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
 		transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
+	}
+
+	void Bob() {
+		if (transform.position.y >= yLimit) {
+			transform.Translate (Vector3.down * bobSpeed);
+		} else if (transform.position.y <= 0f) {
+			transform.Translate (Vector3.up * bobSpeed);
+		}
+
 	}
 
 	void OnTriggerEnter(Collider col) {
