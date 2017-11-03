767a7d52a682243f95ed019e03144734aa02bbb7
asantiago33 <asantiago33@gatech.edu>
2017-11-03 18:24:23 -0400

    Latest commit

--- Assets/ExperimentalStuff/BuildManager.cs
+++ Assets/ExperimentalStuff/BuildManager.cs
@@ -35,6 +35,7 @@ public class BuildManager : MonoBehaviour {
 		normalColor = simpleTurret.color;
 
 		LazerImage.color = selectedColor;
+		BlasterImage.color = selectedColor;
 		simpleTurret.color = normalColor;
 	}
 
@@ -44,11 +45,18 @@ public class BuildManager : MonoBehaviour {
 			thingThatsBuilt = building;
 			simpleTurret.color = normalColor;
 			LazerImage.color = selectedColor;
+			BlasterImage.color = selectedColor;
 		} else if (Input.GetKeyDown(KeyCode.Alpha2)) {
 			thingThatsBuilt = Lazer;
 			LazerImage.color = normalColor;
 			simpleTurret.color = selectedColor;
-		} 
+			BlasterImage.color = selectedColor;
+		} else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+			thingThatsBuilt = Blaster;
+			LazerImage.color = selectedColor;
+			simpleTurret.color = selectedColor;
+			BlasterImage.color = normalColor;
+		}
 
 		if (Input.GetKeyDown(KeyCode.Mouse0)) {
 			if (tile.GetComponent<TileSelectController>().isHighlighted && !isPaused && !tile.GetComponent<TileSelectController>().hasBuilding) {
